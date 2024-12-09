ab2a10e3e2fc9510d0ebbf4f1703ecd915b929e5
E-specter <especter.o.a@gmail.com>
2024-12-08 19:58:06 -0500

    Subida de la demo del programa EventPro

---
+++ EventPro/src/main/java/dev/Lovelace/EventPro/EventPro.java
@@ -0,0 +1,18 @@
+package dev.Lovelace.EventPro;
+
+import javax.swing.JFrame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPro extends JFrame {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(EventPro.class, args);
+    
+    
+    
+  }
+
+}
